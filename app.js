8a5b0195af84953c785aa4651d4dfdfb14f826ac
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-01-26 14:52:01 +0500

    Refactor char count funtionality

--- app.js
+++ app.js
@@ -4,42 +4,18 @@ const charCount = document.querySelector(".char-count");
 const inputField = document.querySelector(".text-input");
 const twitterLimit = document.querySelector(".twitter-count");
 const facebookLimit = document.querySelector(".facebook-count");
-// HELPER FUNCTIONS
-const countChars = function (str) {
-  const filteredStr = str.split("").filter((char) => char !== "\n");
-  return filteredStr.length;
-};
 
-const countWords = function (str) {
-  if (str !== "") {
-    return str.trim().split(" ").length;
-  } else {
-    return 0;
-  }
-};
+inputField.addEventListener("input", () => {
+  // find the number of chars in the textarea
+  const chars = inputField.value.length;
+  const twitterCharsLeft = 280 - chars;
+  const facebookCharsLeft = 2200 - chars;
 
-inputField.addEventListener("keyup", () => {
-  charCount.textContent = countChars(inputField.value);
+  charCount.textContent = chars;
+  twitterLimit.textContent = twitterCharsLeft;
+  facebookLimit.textContent = facebookCharsLeft;
 });
 
-inputField.addEventListener("paste", () => {
-  charCount.textContent = countChars(inputField.value);
-
-  const chars = countChars(inputField.value);
-  twitterLimit.textContent = 280 - chars;
-  facebookLimit.textContent = 2200 - chars;
-});
-
-inputField.addEventListener("keyup", () => {
+inputField.addEventListener("input", () => {
   wordCount.textContent = countWords(inputField.value);
-});
-
-inputField.addEventListener("keyup", () => {
-  const chars = countChars(inputField.value);
-  twitterLimit.textContent = 280 - chars;
-});
-
-inputField.addEventListener("keyup", () => {
-  const chars = countChars(inputField.value);
-  facebookLimit.textContent = 2200 - chars;
 });
